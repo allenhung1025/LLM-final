6dbced4cb30fcb3db07e9050d2a70ae774c902a9
Ubuntu <ubuntu@ip-172-31-32-10.ec2.internal>
2024-11-30 16:02:33 +0000

    update inference code and inference the leetcode dataset

--- src/inference.py
+++ src/inference.py
@@ -5,22 +5,31 @@ from tqdm import tqdm
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from utils.determine_device import determine_device
+from datasets import load_dataset
 
 
 MODEL_MAP = {
     "llama38": "meta-llama/Meta-Llama-3-8B",
-    "gemma": "google/gemma-7b"
+    "gemma": "google/gemma-7b",
+    "salesforce": "Salesforce/codegen-350M-mono"
 }
 
 
-def prepare_messages(file_path: str, model_type: str) -> List[Union[str, Dict[str, str]]]:
-    with open(file_path, "r") as f:
-        data = [json.loads(line) for line in f]
+# def prepare_messages(file_path: str, model_type: str) -> List[Union[str, Dict[str, str]]]:
+#     with open(file_path, "r") as f:
+#         data = [json.loads(line) for line in f]
     
-    if model_type == "pretrained":
-        return [item["prompt"] for item in data]
-    else:
-        return [{"role": "user", "content": item["prompt"]} for item in data]
+#     if model_type == "pretrained":
+#         return [item["prompt"] for item in data]
+#     else:
+#         return [{"role": "user", "content": item["prompt"]} for item in data]
+def prepare_messages(title: str, description: str, python_clean: str) -> List[Union[str, Dict[str, str]]]:
+    # with open(file_path, "r") as f:
+    #     data = [json.loads(line) for line in f]
+    res = []
+    for ti, des, python in zip(title, description, python_clean):
+        res.append({"description": f"generate python code with the following description : {des}", "python_clean": python, "title": ti})
+    return res
 
 
 @torch.inference_mode()
@@ -30,29 +39,35 @@ def generate_output(
     messages: List[Union[str, Dict[str, str]]],
     output_file: str,
     model_type: str,
-    device: str
+    device: str,
+    batch_size: int = 32,
 ):
     with open(output_file, "w") as f:
-        for message in tqdm(messages):
-            prompt = message if isinstance(message, str) else message["content"]
+        for i in tqdm(range(0, len(messages), batch_size)):
+            batch_messages = messages[i:i+batch_size]
+            batch_prompts = [message if isinstance(message, str) else message["description"] for message in batch_messages]
             
-            if model_type == "pretrained":
-                inputs = tokenizer(prompt, return_tensors='pt', return_token_type_ids=False)
-            else:
-                inputs = tokenizer.apply_chat_template([message], tokenize=True, add_generation_prompt=True, return_tensors="pt")
+           
+            inputs =  inputs = tokenizer(
+                batch_prompts, 
+                return_tensors='pt', 
+                return_token_type_ids=False, 
+                padding=True, 
+                truncation=True
+            )
+            #print(inputs.shape)
+           
 
-            if model_type != "pretrained" and not isinstance(inputs, dict):
-                inputs = {'input_ids': inputs}
+            # if model_type != "pretrained" and not isinstance(inputs, dict):
+            #     inputs = {'input_ids': inputs}
             inputs = {k: v.to(device) for k, v in inputs.items()}
             
             response = model.generate(**inputs, max_new_tokens=300)
-            response_decoded = tokenizer.batch_decode(response, skip_special_tokens=True)[0]
+            response_decoded = tokenizer.batch_decode(response, skip_special_tokens=True)
 
-            if model_type != "pretrained":
-                response_decoded = response_decoded.split("\n<|assistant|>\n")[-1]
-
-            json.dump({"prompt": prompt, "output": response_decoded}, f)
-            f.write("\n")
+            for j, message in enumerate(batch_messages):
+                json.dump({"title": message["title"], "prompt": batch_prompts[j], "output": response_decoded[j], "groundtruth": message["python_clean"]}, f)
+                f.write("\n")
 
 
 def main():
@@ -76,8 +91,18 @@ def main():
         required=True,
         help="A jsonl file where each line will have the input and the corresponding output"
     )
+    parser.add_argument(
+        "--batch_size",
+        type=int,
+        default=8,
+        help="Number of prompts to process in each batch"
+    )
     args = parser.parse_args()
-
+    dataset = load_dataset("allenhung1025/leetcode")
+    title = dataset['train']["title"]
+    python = dataset['train']["python_clean"]
+    content = dataset['train']['content']
+    # import pdb; pdb.set_trace()
     device = determine_device()
     model = AutoModelForCausalLM.from_pretrained(
         MODEL_MAP[args.model_type], 
@@ -85,9 +110,9 @@ def main():
     ).to(device)
     model.eval()
     tokenizer = AutoTokenizer.from_pretrained(MODEL_MAP[args.model_type])
-
-    messages = prepare_messages(args.prompts, args.model_type)
-    generate_output(model, tokenizer, messages, args.output, args.model_type, device)
+    tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+    messages = prepare_messages(title, content, python)
+    generate_output(model, tokenizer, messages, args.output, args.model_type, device, args.batch_size)
 
     print("Done!")
 
